cb0d905437a8a4535a53e98f57e6d01f846a3d2b
devfelixarthur <de.felixarthur@gmail.com>
2022-07-26 20:45:59 -0300

    revisao exercio a009

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,7 +3,7 @@ const comida1 = prompt ('Qual a sua comida Favorita ?')
 const comida2 = prompt ('Qual a sua segunda comida Favorita ?')
 const comida3 = prompt ('Qual a sua terceira comida Favorita ?')
 
-console.log (`Qual a sua comida favorita: ${comida1}
-\nQual a sua segudna comida favorita: ${comida2}
-\nQual a sua terceira comida favorita: ${comida3}`)
-
+console.log (`Estas são as comidas favoritas de ${nome}:
+\n- ${comida1}
+\n- ${comida2}
+\n- ${comida3}`)
