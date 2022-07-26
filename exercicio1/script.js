0a7bf31c335b699fbc780b71673cc4d4fd7292ca
devfelixarthur <de.felixarthur@gmail.com>
2022-07-25 22:57:24 -0300

    Exercicio Fixação Aula a009

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const nome = prompt ('Qual o seu nome?')
+const comida1 = prompt ('Qual a sua comida Favorita ?')
+const comida2 = prompt ('Qual a sua segunda comida Favorita ?')
+const comida3 = prompt ('Qual a sua terceira comida Favorita ?')
+
+console.log (`Qual a sua comida favorita: ${comida1}
+\nQual a sua segudna comida favorita: ${comida2}
+\nQual a sua terceira comida favorita: ${comida3}`)
+
